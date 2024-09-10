581aee0803615075b513541be5994bdc43e4dc77
Chavirez <santiagosanchezch@gmail.com>
2024-09-09 20:55:45 -0700

    Update NegocioException.java

--- src/main/java/Negocio/NegocioException.java
+++ src/main/java/Negocio/NegocioException.java
@@ -1,5 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package negocio;
 
+/**
+ *
+ * @author PC
+ */
 public class NegocioException extends Exception {
 
     public NegocioException(String message) {
